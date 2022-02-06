a2faaad6919a2f8693f9481fcfedafe0b6a78e6e
zack alsiday <alsidayzack@gmail.com>
2022-02-06 13:40:35 -0800

    setup login router

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
 const bcrypt = require("bcryptjs")
+
+
 router.get("/test",(req,res) => {
     res.json({msg: 'this is the user route'})
 })
+
+
 
 router.post('/register', (req,res) => {
     User.findOne({email: req.body.email})
@@ -29,5 +33,27 @@ router.post('/register', (req,res) => {
             })
         }
     })
+})
+
+router.post('/login', (req,res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email}) 
+        .then(user => {
+            if(!user){
+                return res.status(404).json({email: "this user does not exist."});
+            }
+
+            bcrypt.compare(password, user.password) 
+                .then(isMatch => {
+                    if(isMatch) {
+                          res.json({msg: "Success!"})
+                    } else {
+                        return res.status(400).json({password: "Incorrect password"});
+                    }
+                  
+                })
+        })
 })
 module.exports = router; 
