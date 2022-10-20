dc2cc23ff91dae5f1eec61e75154e2c6f5ebd6c2
zack alsiday <alsidayzack@gmail.com>
2022-10-20 10:47:57 -0700

    created user validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,15 +7,22 @@ const jwt = require('jsonwebtoken');
 // this allows users to sign in and access protected routes.
 const keys = require('../../config/keys');
 // router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // SIGN UP ROUTE
 router.post('/register', (req, res) => {
+    const {errors, isValid} = validateRegisterInput(req.body)
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
     // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
                 // Throw a 400 error if the email address already exists
-                return res.status(400).json({ email: "A user has already registered with this address" })
+                errors.email = 'Email already exists'
+                return res.status(400).json(errors)
             } else {
                 // Otherwise create a new user
                 const newUser = new User({
@@ -51,11 +58,15 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-
+    const { errors, isValid } = validateLoginInput(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
     User.findOne({ email })
         .then(user => {
             if (!user) {
-                return res.status(404).json({ email: 'This user does not exist' });
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
 
             bcrypt.compare(password, user.password)
@@ -71,16 +82,20 @@ router.post('/login', (req, res) => {
                         // res.json({ msg: 'Success' });
                     } else {
                 
-                        return res.status(400).json({password: 'Incorrect password'});
+                        errors.password = 'Incorrect password'
+                        return res.status(400).json(errors);
                     }
                 })
         })
 })
 
 // PRIVATE AUTH ROUTH
-router.get('/current', passport.authenticate('jwt', { session: false, failureMessage: true }), (req, res) => {
-
-    res.json({ msg: 'Success' });
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        handle: req.user.handle,
+        email: req.user.email
+    });
 })
 
 module.exports = router;
